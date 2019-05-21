c5504f0fe73e69a5acfce1c072ca3043a5af0c92
Cristian Veras <mrchrist85@gmail.com>
2019-05-21 00:17:45 -0400

    Initial commit

---
+++ app/src/main/java/com/cv5/holamundo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.cv5.holamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView texto;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        texto = findViewById(R.id.texto);
+    }
+    public void cambiarTexto(){
+        texto.setText("Hola mundo!");
+
+    }
+}
